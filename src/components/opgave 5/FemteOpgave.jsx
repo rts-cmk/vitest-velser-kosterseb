f423f5d81a44ab34d603a9f9dfdaecc180cb041f
kosterseb <sebkos.code@gmail.com>
2025-11-12 09:57:20 +0100

    just added some edits to the fifth assignment

--- src/components/opgave 5/FemteOpgave.jsx
+++ src/components/opgave 5/FemteOpgave.jsx
@@ -1,4 +1,3 @@
-
 import { eksternAfhaengighed } from './eksternAfhaengighed';
 export function FemteOpgave() {
   return eksternAfhaengighed();
--- src/components/opgave 6/SjetteOpgave.jsx
+++ src/components/opgave 6/SjetteOpgave.jsx
@@ -1,4 +1,1 @@
-// Øvelse 6: Testdækning og refaktorering
-// Brug Vitests dækningsoverblik til at finde manglende tests.
-// Tilføj tests, så alle grene i funktionerne bliver dækket.
-// Refaktorér koden og kontroller, at alle tests stadig består.
+
